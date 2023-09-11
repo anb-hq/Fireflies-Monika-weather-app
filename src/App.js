4902a7b09b87dab7aded443ef3b8f67e6a23529e
monikaengineering <rithmonika222115@gmail.com>
2023-09-11 10:29:24 +0700

    fixing file

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src = "lighting.jpg" alt = "lighting" />
         <p>
           <code>Create-weather-app.</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
       </header>
     </div>
   );
